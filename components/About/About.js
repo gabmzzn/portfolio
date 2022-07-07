20cec9d161dcb23678cde7f26e4b733eb1aca255
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-07-07 14:36:54 -0300

    asd

--- components/About/About.js
+++ components/About/About.js
@@ -5,49 +5,55 @@ import css from './About.module.css'
 const About = (props) => {
 
 	const { about } = props
-	const { name, role, description, resume, social } = about
+	const { role, description, resume, social } = about
+
 
 	return (
-		<div className={css.about}>
-			<h2 className={css.hi}>
-				Hi, my name is
-			</h2>
-			<h1>
-				<span className={css.name}>
-					Juan Gabriel <br />Mazzoleni
-				</span>
-			</h1>
-
-			{role && <h2 className={css.role}>{role}</h2>}
-			<p className={css.desc}>{description}</p>
-
-			<div className={`${css.contact} center`}>
-				<a
-					href={social.github}
-					aria-label='github'
-					className={`${css.linkIcon} link`}
-					target="_blank" rel="noreferrer"
-				>
-					<h5>GitHub</h5>
-					<GitHubIcon />
-				</a>
-				<a
-					href={social.linkedin}
-					aria-label='linkedin'
-					className={`${css.linkIcon} link`}
-					target="_blank" rel="noreferrer"
-				>
-					<h5>LinkedIn</h5>
-					<LinkedInIcon />
-				</a>
-				<a href={resume}
-					aria-label='github'
-					target="_blank" rel="noreferrer"
-				>
-					<span type='button' className={`${css.btnOutline} btn`}>
-						Resume / cv
+		<div>
+			<div className={css.about}>
+				<h2 className={css.hi}>
+					Hi, my name is
+				</h2>
+				<h1>
+					<span className={css.name}>
+						Juan Gabriel <br />Mazzoleni
 					</span>
-				</a>
+				</h1>
+
+				{role && <h2 className={css.role}>{role}</h2>}
+				<p className={css.desc}>
+					I live in Cordoba, Argentina, and I'm a passionate multi-skilled <br />
+					developer and information systems engineer student
+				</p>
+
+				<div className={`${css.contact} center`}>
+					<a
+						href={social.github}
+						aria-label='github'
+						className={`${css.linkIcon} link`}
+						target="_blank" rel="noreferrer"
+					>
+						<h5>GitHub</h5>
+						<GitHubIcon />
+					</a>
+					<a
+						href={social.linkedin}
+						aria-label='linkedin'
+						className={`${css.linkIcon} link`}
+						target="_blank" rel="noreferrer"
+					>
+						<h5>LinkedIn</h5>
+						<LinkedInIcon />
+					</a>
+					<a href={resume}
+						aria-label='github'
+						target="_blank" rel="noreferrer"
+					>
+						<span type='button' className={`${css.btnOutline} btn`}>
+							Resume / cv
+						</span>
+					</a>
+				</div>
 			</div>
 		</div>
 	)
