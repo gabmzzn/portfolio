638f990627912a51a0e7f8364f59b0ed06ae5274
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-07-07 01:18:27 -0300

    p

--- data/portfolio.js
+++ data/portfolio.js
@@ -5,7 +5,7 @@ const header = {
 
 const about = {
 	name: 'Juan Gabriel Mazzoleni',
-	role: 'a Full Stack Developer',
+	role: 'Full Stack Developer',
 	description:
 		'I live in Cordoba, Argentina, and I\'m a passionate multi-skilled developer and an information systems engineer student',
 	resume: '/CV - Juan Gabriel Mazzoleni.pdf',
