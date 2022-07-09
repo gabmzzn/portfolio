0949061c24d2f2ed3c5bd1b1789f5bc547cb4216
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-07-09 01:24:36 -0300

    shadow shadow

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,15 +5,19 @@ import css from './Header.module.css'
 const Header = () => {
 
 	const [scroll, setScroll] = useState(false)
+	const [shadow, setShadow] = useState(false)
 
 	useEffect(() => {
 		window.addEventListener("scroll", () => {
 			setScroll(window.scrollY > 900)
+			setShadow(window.scrollY > 30)
 		})
 	}, [])
 
 	return (
-		<header className={`${css.header} ${scroll ? css.shadow : ''}`}>
+		<header className={`${css.header} 
+		${scroll ? css.navbarDisplay : ''}
+		${shadow ? css.shadow : ''}`}>
 			<Navbar />
 		</header>
 	)
