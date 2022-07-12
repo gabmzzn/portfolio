0cd6daaf321262819f9dea202de5b1a0d4588c16
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-07-12 13:25:08 -0300

    ok

--- data/portfolio.js
+++ data/portfolio.js
@@ -23,7 +23,7 @@ const experience = [
 		time: 'Contract - Sept 2020 · Present',
 		description:
 			'Developer of a Google Chrome extension in partnership with an advertising company with an initial floor of 30.000 users taken successfully to +70.000 users.',
-		stack: ['React.js', 'TypeScript', 'Javascript', 'Chrome API', 'HTML', 'CSS/SASS'],
+		stack: ['Javascript', 'Chrome API', 'HTML', 'CSS/SASS'],
 		livePreview: 'https://zoomtofill.web.app/',
 	},
 	{
